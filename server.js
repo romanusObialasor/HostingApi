f4d6fe07a0be3c276cbf08928822ee3319c5f31d
romanusObialasor <robialasor5@gmail.com>
2021-08-18 11:53:14 +0100

    Newly Updated Item

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 // const express = require("express");
 // const mongoose = require("mongoose");
 // const port = process.env.PORT || 5000;
@@ -30,7 +30,7 @@
 // });
 
 const express = require("express");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const studentData = [
   { id: 1, name: "Romanus", course: "Javascript" },
